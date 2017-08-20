c725c65826b9da03ac345a12e3c6cb8012578b72
Manvi Gupta <manvigupta1987@gmail.com>
2017-08-20 14:42:13 -0400

    Fix:Change to remove extra class from BuildDesignPattern

--- DesignPatterns/BuildDesignPatternExample1/src/com/build/house/House.java
+++ DesignPatterns/BuildDesignPatternExample1/src/com/build/house/House.java
@@ -1,31 +1,29 @@
 package com.build.house;
 
-public class House implements HousePlan{
+public class House {
 
 	private String basement;
 	private String floor;
 	private String roof;
 	private String interior;
 	
-	@Override
 	public void setBasement(String basement) {
 		// TODO Auto-generated method stub
 		this.basement = basement;
 	}
 
-	@Override
 	public void setfloor(String floor) {
 		// TODO Auto-generated method stub
 		this.floor = floor;
 	}
 
-	@Override
+	
 	public void setRoof(String roof) {
 		// TODO Auto-generated method stub
 		this.roof = roof;
 	}
 
-	@Override
+
 	public void setInterior(String interior) {
 		// TODO Auto-generated method stub
 		this.interior = interior;
